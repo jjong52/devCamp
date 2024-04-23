c93c567f8159a87ac4fdef6e04df52bd9d19f7a3
Suin <tktw555@gmail.com>
2024-04-23 21:54:23 +0900

    게시글 작성 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
 
     @Get('/')
-    getAllBoard() {
+    getAllBoard(): Board[] {
         return this.boardService.getAllBoards();
+    }
+
+    @Post('/')
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardService.createBoard(createBoardDto);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { Board, BoardStatus } from './board.model';
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title, // 매개변수명과 필드명이 같을 땐 title: title을 그냥 title로 줄일 수 있다.
+            description,
+            status: BoardStatus.PUBLIC,
+        }
+        this.boards.push(board);
+        return board;
     }
 }
